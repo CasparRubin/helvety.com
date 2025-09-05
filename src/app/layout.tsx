de85f358f4140d3382fe7a108030a73a9e972c4d
Caspar Rubin <caspar@helvety.com>
2025-09-05 17:08:57 +0200

    added vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -48,6 +49,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${geistSans.variable} antialiased`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
