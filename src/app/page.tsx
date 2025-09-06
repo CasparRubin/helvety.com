0a905379a814f609550e8ada2823e57914d349f3
Caspar Rubin <caspar@helvety.com>
2025-09-06 12:20:26 +0200

    adjusted some spacing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,13 +4,13 @@ export default function Home() {
   return (
     <main className="min-h-screen">
       <div className="max-w-5xl mx-auto px-6 py-20">
-        <div className="text-center space-y-12">
+        <div className="text-center">
           <Image
             src="/logo_whiteBg.svg"
             alt="Helvety logo"
             width={4000}
             height={1000}
-            className="w-full h-auto"
+            className="w-full h-auto mb-24"
             priority
           />
           <div className="space-y-2">
@@ -19,7 +19,7 @@ export default function Home() {
             </p>
             <p className="text-sm md:text-base text-black/70">developed and designed in <span className="text-[#FF0000]">Switzerland</span></p>
           </div>
-          <div className="border-t border-black/30" />
+          <div className="border-t border-black/30 mt-24 mb-8" />
           <p className="text-base md:text-lg text-black/50">contact@helvety.com</p>
         </div>
       </div>
